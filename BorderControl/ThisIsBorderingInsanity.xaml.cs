40906a5313858d69867dd653b1e245006737f5af
User <User@LAB3-3>
2024-01-10 10:18:50 +0200

    Made all buttons but =

--- BorderControl/ThisIsBorderingInsanity.xaml.cs
+++ BorderControl/ThisIsBorderingInsanity.xaml.cs
@@ -6,4 +6,32 @@ public partial class ThisIsBorderingInsanity : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Print(object sender, EventArgs e)
+    {
+		Button btn = (Button)sender;
+		lbl.Text += btn.Text;
+    }
+	private void MethodPrint(object sender, EventArgs e)
+	{
+		if (lbl.Text[lbl.Text.Length - 1] != '.'
+			&& lbl.Text[lbl.Text.Length - 1] != '+'
+			&& lbl.Text[lbl.Text.Length - 1] != '-'
+			&& lbl.Text[lbl.Text.Length - 1] != '*'
+			&& lbl.Text[lbl.Text.Length - 1] != '/'
+			&& lbl.Text[lbl.Text.Length - 1] != '%'
+			&& lbl.Text[lbl.Text.Length - 1] != '!')
+		{
+            Button btn = (Button)sender;
+            lbl.Text += btn.Text;
+        }
+	}
+	private void Delete(object sender, EventArgs e)
+	{
+		if (lbl.Text.Length>0) lbl.Text=lbl.Text.Remove(lbl.Text.Length-1);
+	}
+	private void Reset(object sender, EventArgs e)
+	{
+		lbl.Text = "";
+	}
 }
